b541e127f92b1ab9669b3fa48a99536c1cc89ab9
ittimfn <ittimfn+github@gmail.com>
2022-04-07 23:36:19 +0900

    errorページ作成

--- src/main/java/sample/springboot/SocialApplication.java
+++ src/main/java/sample/springboot/SocialApplication.java
@@ -9,11 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-
 import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpServletRequest;
 
 import java.util.Map;
 import java.util.Collections;
@@ -23,11 +25,20 @@ import java.lang.Exception;
 @RestController
 public class SocialApplication extends WebSecurityConfigurerAdapter {
 
+   SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
     @GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         return Collections.singletonMap("name", principal.getAttribute("name"));
     }
     
+    @GetMapping("/error")
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
@@ -45,7 +56,12 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
             .exceptionHandling(e -> e
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
             )
-            .oauth2Login();
+            .oauth2Login(o -> o
+                .failureHandler((request, response, exception) -> {
+                    request.getSession().setAttribute("error.message", exception.getMessage());
+                    handler.onAuthenticationFailure(request, response, exception);
+                })
+            );
         // @formatter:on
     }
 
